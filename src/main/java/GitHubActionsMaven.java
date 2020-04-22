5a4980378b9576c63052423d7f3a3e8d5c2e1e53
bigdra50 <ryu1998tea@gmail.com>
2020-04-23 05:16:45 +0900

    wip

--- src/main/java/GitHubActionsMaven.java
+++ src/main/java/GitHubActionsMaven.java
@@ -1,7 +1,7 @@
 
-public class GitHubActionsTest {
+public class GitHubActionsMaven {
     public static void main(String[] args){
-        System.out.println("Helloooooo");
+        System.out.println("Hello");
     }
 
     public static int add(int x, int y){
--- src/test/java/GitHubActionsTestTest.java
+++ src/test/java/GitHubActionsTestTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class GitHubActionsTestTest {
 
     @Test
-    void add() {
-        assertEquals(5, GitHubActionsTest.add(2, 3));
+    void addTest() {
+        assertEquals(4, GitHubActionsMaven.add(2, 3));
     }
 
     @Test
-    void mul() {
-        assertEquals(6, GitHubActionsTest.mul(2, 3));
+    void mulTest() {
+        assertEquals(6, GitHubActionsMaven.mul(2, 3));
     }
 }
